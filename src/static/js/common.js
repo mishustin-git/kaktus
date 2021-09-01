fd7d04ec2f75b4e307ca54402ca6a03e60a7d6f7
mishustin-git <mishustin.post@yandex.ru>
2021-09-01 17:59:40 +0300

    too many elements in header

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-    // alert(1);
+    if (($(window).width()<1024) && $(window).width()>768){
+    $("#disclose").on("focus",function(){
+        $(".actions-icons").css("display","none");
+        $(".find-bar").css("margin-right","0");
+    });
+    $("#disclose").on("focusout",function(){
+        $(".actions-icons").css("display","inline-flex");
+        $(".find-bar").css("margin-right","66px");
+    });
+    }
 });
