4738b19640767fc0d52a3890edf1b890d37e5404
mishustin-git <mishustin.post@yandex.ru>
2021-09-02 17:47:41 +0300

    header & footer done, working on menu

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -9,4 +9,18 @@ $(document).ready(function () {
         $(".find-bar").css("margin-right","66px");
     });
     }
+    // 
+    $(".sticky-block a").on("click",function(){
+        $(".sticky-block a.active").toggleClass("active");
+      $(this).toggleClass("active");
+    })
+    // 
+    if ($(window).width()<769){
+        $('body').height($('body').height()+$(".sticky-block").height()+24);
+        $(".nav-block").on("click",function(){
+            if (!$(this).hasClass("opened"))
+                $(".nav-block.opened").toggleClass("opened");
+        $(this).toggleClass("opened");
+        })
+    }
 });
